37f6e57de8c1b1b644fb5a9f0603a350d3847b77
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-12-22 22:09:37 +0900

    Update

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,19 @@ end
 @testset "JET" begin
     JET.report_package(QuartzGetWindow, target_defined_modules=true)
 end
+
+@testset "getAllActiveWindowNames" begin
+    @test getAllActiveWindowNames() |> length |> ≥(0)
+end
+
+@testset "getActiveWindowGeometry" begin
+    windows = getAllActiveWindowNames()
+    if windows ≥ 1
+        win = windows |> first
+        x,y,w,h = getActiveWindowGeometry(win)
+        x ≥ 0
+        y ≥ 0
+        w ≥ 0
+        h ≥ 0
+    end
+end
